ee5914ea997dfe0369128ed875b1ed50026e4ece
Juga Paazmaya <paazmaya@yahoo.com>
2023-08-27 23:10:28 +0300

    Simplify and use features of Walkdir

--- src/file_counter.rs
+++ src/file_counter.rs
@@ -10,38 +10,25 @@ fn has_valid_extension(file_path: &Path, extensions: &[String]) -> bool {
     }
 }
 
-pub fn find_files_recursive(directory: &Path, extensions: &[String]) -> Vec<PathBuf> {
+pub fn find_files(directory: &Path, extensions: &[String], recursive: bool) -> Vec<PathBuf> {
     let mut matching_files: Vec<PathBuf> = Vec::new();
 
-    for entry in WalkDir::new(directory) {
-        if let Ok(entry) = entry {
-            let path = entry.path();
-            if path.is_file() && has_valid_extension(&path, extensions) {
+    let walker: walkdir::IntoIter = if recursive {
+        WalkDir::new(directory).into_iter()
+    } else {
+        WalkDir::new(directory).max_depth(1).into_iter()
+    };
+
+    for entry in walker.filter_map(|e: Result<walkdir::DirEntry, walkdir::Error>| e.ok()) {
+        if entry.file_type().is_file() {
+            let path: &Path = entry.path();
+            if has_valid_extension(path, extensions) {
                 matching_files.push(path.to_path_buf());
             }
         }
     }
 
     matching_files
-}
-
-// Function to find files non-recursively
-pub fn find_files_non_recursive(directory: &PathBuf, extensions: &[String]) -> Vec<PathBuf> {
-    let mut file_paths: Vec<PathBuf> = Vec::new();
-
-    for entry in directory.read_dir().unwrap() {
-        if let Ok(entry) = entry {
-            if entry.file_type().unwrap().is_file() {
-                if let Some(extension) = entry.path().extension().and_then(|e| e.to_str()) {
-                    if extensions.is_empty() || extensions.contains(&extension.to_string()) {
-                        file_paths.push(entry.path());
-                    }
-                }
-            }
-        }
-    }
-
-    file_paths
 }
 
 // Unit tests for the functions
@@ -62,88 +49,87 @@ mod tests {
 
     #[test]
     fn test_has_valid_extension_valid() {
-        let file_path = Path::new("test.txt");
-        let extensions = vec!["txt".to_string(), "jpg".to_string()];
-        assert!(has_valid_extension(&file_path, &extensions));
+        let file_path: &Path = Path::new("test.txt");
+        let extensions: Vec<String> = vec!["txt".to_string(), "jpg".to_string()];
+        assert!(has_valid_extension(file_path, &extensions));
     }
 
     #[test]
     fn test_has_valid_extension_invalid() {
-        let file_path = Path::new("test.png");
-        let extensions = vec!["txt".to_string(), "jpg".to_string()];
-        assert!(!has_valid_extension(&file_path, &extensions));
+        let file_path: &Path = Path::new("test.png");
+        let extensions: Vec<String> = vec!["txt".to_string(), "jpg".to_string()];
+        assert!(!has_valid_extension(file_path, &extensions));
     }
 
     #[test]
     fn test_has_valid_extension_no_extension() {
-        let file_path = Path::new("test");
-        let extensions = vec!["txt".to_string(), "jpg".to_string()];
-        assert!(!has_valid_extension(&file_path, &extensions));
+        let file_path: &Path = Path::new("test");
+        let extensions: Vec<String> = vec!["txt".to_string(), "jpg".to_string()];
+        assert!(!has_valid_extension(file_path, &extensions));
     }
 
     #[test]
     fn test_find_files_recursive_no_files() {
-        let directory = PathBuf::from("test_data/empty_directory");
-        let extensions = vec!["txt".to_string()];
-        let result = find_files_recursive(&directory, &extensions);
+        let directory: PathBuf = PathBuf::from("test_data/empty_directory");
+        let extensions: Vec<String> = vec!["txt".to_string()];
+        let result: Vec<PathBuf> = find_files(&directory, &extensions, true);
         assert_eq!(result.len(), 0);
     }
 
     #[test]
     fn test_find_files_recursive_some_files() {
-        let directory = PathBuf::from("test_data/non_empty_directory");
-        let extensions = vec!["txt".to_string()];
-        let result = find_files_recursive(&directory, &extensions);
+        let directory: PathBuf = PathBuf::from("test_data/non_empty_directory");
+        let extensions: Vec<String> = vec!["txt".to_string()];
+        let result: Vec<PathBuf> = find_files(&directory, &extensions, true);
         assert_eq!(result.len(), 1);
     }
 
     #[test]
     fn test_find_files_recursive_subdirectory() {
-        let directory = PathBuf::from("test_data/non_empty_directory");
-        let extensions = vec!["png".to_string()];
-        let result = find_files_recursive(&directory, &extensions);
+        let directory: PathBuf = PathBuf::from("test_data/non_empty_directory");
+        let extensions: Vec<String> = vec!["png".to_string()];
+        let result: Vec<PathBuf> = find_files(&directory, &extensions, true);
         assert_eq!(result.len(), 1);
     }
 
     #[test]
     fn test_find_files_recursive_multiple_extensions() {
-        let directory = PathBuf::from("test_data/non_empty_directory");
-        let extensions = vec!["txt".to_string(), "jpg".to_string()];
-        let result = find_files_recursive(&directory, &extensions);
+        let directory: PathBuf = PathBuf::from("test_data/non_empty_directory");
+        let extensions: Vec<String> = vec!["txt".to_string(), "jpg".to_string()];
+        let result: Vec<PathBuf> = find_files(&directory, &extensions, true);
         assert_eq!(result.len(), 2);
     }
 
     #[test]
     fn test_find_files_non_recursive_no_files() {
-        let directory = PathBuf::from("test_data/empty_directory");
-        let extensions = vec!["txt".to_string()];
-        let result = find_files_non_recursive(&directory, &extensions);
+        let directory: PathBuf = PathBuf::from("test_data/empty_directory");
+        let extensions: Vec<String> = vec!["txt".to_string()];
+        let result: Vec<PathBuf> = find_files(&directory, &extensions, false);
         assert_eq!(result.len(), 0);
     }
 
     #[test]
     fn test_find_files_non_recursive_some_files() {
-        let directory = PathBuf::from("test_data/non_empty_directory");
-        let extensions = vec!["txt".to_string()];
-        let result = find_files_non_recursive(&directory, &extensions);
+        let directory: PathBuf = PathBuf::from("test_data/non_empty_directory");
+        let extensions: Vec<String> = vec!["txt".to_string()];
+        let result: Vec<PathBuf> = find_files(&directory, &extensions, false);
         assert_eq!(result.len(), 1);
     }
 
     #[test]
     fn test_find_files_non_recursive_subdirectory() {
-        let directory = PathBuf::from("test_data/non_empty_directory");
-        let extensions = vec!["png".to_string()];
-        let result = find_files_non_recursive(&directory, &extensions);
+        let directory: PathBuf = PathBuf::from("test_data/non_empty_directory");
+        let extensions: Vec<String> = vec!["png".to_string()];
+        let result: Vec<PathBuf> = find_files(&directory, &extensions, false);
         assert_eq!(result.len(), 0);
     }
 
     #[test]
     fn test_find_files_non_recursive_multiple_extensions() {
-        let directory = PathBuf::from("test_data/non_empty_directory");
-        let extensions = vec!["txt".to_string(), "jpg".to_string()];
-        let result = find_files_non_recursive(&directory, &extensions);
+        let directory: PathBuf = PathBuf::from("test_data/non_empty_directory");
+        let extensions: Vec<String> = vec!["txt".to_string(), "jpg".to_string()];
+        let result: Vec<PathBuf> = find_files(&directory, &extensions, false);
         assert_eq!(result.len(), 2);
     }
 
-    // Add more test cases as needed
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 mod file_counter;
 use crate::file_counter::*;
 
-
 // Add the necessary imports
 use std::path::PathBuf;
 use clap::StructOpt;
@@ -21,9 +20,9 @@ struct Cli {
 
 fn main() {
     // Default list of common image and video extensions
-    let default_extensions: Vec<String> = vec![
+    let default_extensions: Vec<String> = [
         "jpg", "jpeg", "png", "gif", "bmp", // Image formats
-        "mp4", "avi", "mkv", "mov", "wmv", // Video formats
+        "mp4", "avi", "mkv", "mov", "wmv" // Video formats
     ]
     .iter()
     .map(|&ext| ext.to_string())
@@ -33,21 +32,16 @@ fn main() {
     let args: Cli = Cli::from_args();
 
     // Combine user-provided extensions with default extensions
-    let user_extensions: Vec<String> = args.extensions.iter().cloned().collect::<Vec<_>>();
+    let user_extensions: Vec<String> = args.extensions.to_vec();
     let all_extensions: Vec<String> = combine_extensions(user_extensions, default_extensions);
 
     // Call the function to find files
-    let matching_files: Vec<PathBuf> = if args.recursive {
-        find_files_recursive(&args.directory, &all_extensions)
-    } else {
-        find_files_non_recursive(&args.directory, &all_extensions)
-    };
+    let matching_files: Vec<PathBuf> = find_files(&args.directory, &all_extensions, args.recursive);
 
     let total_files: usize = matching_files.len();
 
     println!("Total files found: {}", total_files);
 }
-
 
 fn combine_extensions(user_extensions: Vec<String>, default_extensions: Vec<String>) -> Vec<String> {
     let mut all_extensions: Vec<String> = default_extensions;
@@ -70,27 +64,40 @@ mod tests {
 
     #[test]
     fn test_combine_extensions_with_user_extensions() {
-        let user_extensions = vec!["png".to_string(), "gif".to_string()];
-        let default_extensions = vec!["txt".to_string(), "jpg".to_string()];
-        let result = combine_extensions(user_extensions, default_extensions);
+        let user_extensions: Vec<String> = vec!["png".to_string(), "gif".to_string()];
+        let default_extensions: Vec<String> = vec!["txt".to_string(), "jpg".to_string()];
+        let result: Vec<String> = combine_extensions(user_extensions, default_extensions);
         assert_eq!(result, vec!["txt", "jpg", "png", "gif"]);
     }
 
     #[test]
     fn test_combine_extensions_only_default_extensions() {
-        let user_extensions = vec![];
-        let default_extensions = vec!["txt".to_string(), "jpg".to_string()];
-        let result = combine_extensions(user_extensions, default_extensions);
+        let user_extensions: Vec<String> = vec![];
+        let default_extensions: Vec<String> = vec!["txt".to_string(), "jpg".to_string()];
+        let result: Vec<String> = combine_extensions(user_extensions, default_extensions);
         assert_eq!(result, vec!["txt", "jpg"]);
     }
 
     #[test]
     fn test_combine_extensions_with_duplicates() {
-        let user_extensions = vec!["txt".to_string(), "jpg".to_string()];
-        let default_extensions = vec!["jpg".to_string(), "png".to_string()];
-        let result = combine_extensions(user_extensions, default_extensions);
+        let user_extensions: Vec<String> = vec!["txt".to_string(), "jpg".to_string()];
+        let default_extensions: Vec<String> = vec!["jpg".to_string(), "png".to_string()];
+        let result: Vec<String> = combine_extensions(user_extensions, default_extensions);
         assert_eq!(result, vec!["jpg", "png", "txt", "jpg"]);
     }
+    
+    #[test]
+    fn test_combine_extensions() {
+        let user_extensions = vec!["txt".to_string(), "csv".to_string()];
+        let default_extensions = vec!["jpg".to_string(), "png".to_string()];
 
-    // Add more test cases as needed
+        let combined_extensions = combine_extensions(user_extensions.clone(), default_extensions.clone());
+
+        assert_eq!(combined_extensions.len(), user_extensions.len() + default_extensions.len());
+        assert!(combined_extensions.contains(&user_extensions[0]));
+        assert!(combined_extensions.contains(&user_extensions[1]));
+        assert!(combined_extensions.contains(&default_extensions[0]));
+        assert!(combined_extensions.contains(&default_extensions[1]));
+    }
+
 }
